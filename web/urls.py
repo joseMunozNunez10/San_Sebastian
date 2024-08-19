77bba3c589d83a1a7de6b13dde8c017d129e7f0f
Jose Munoz <jose.e.munoz.nunez@gmail.com>
2024-08-19 00:12:59 -0400

    se termina index.html, se termina nabar y footer HTML, comienza a trabajar en about.html

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import IndexView
+from .views import about
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('about/', about, name='about'),
 ]
--- web/views.py
+++ web/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 class IndexView(View):
     def get(self, request):
         return render(request, 'index.html')
+    
+
+class AboutView(View):
+    def get(self, request):
+        return render(request, 'about.html')
